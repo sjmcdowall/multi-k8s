4dc4d80b31d18aba28a52f28535c31e190d90878
Steven McDowall <sjm@hawoods.org>
2021-03-06 12:32:51 -0500

    Push commit

---
+++ client/src/App.test.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+
+// Need to not have this test since there is no mocks for Fib
+test('renders learn react link', () => {})
---
+++ server/index.js
@@ -0,0 +1,86 @@
+const keys = require('./keys')
+
+// Express App Setup
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+// Postgres Client Setup
+const { Pool } = require('pg')
+const pgPool = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+})
+
+var pgClient
+
+// Capture and log any errors
+pgPool.on('error', (err, client) => {
+  console.log('API: Unexpected error on Postgres Pool idle client', err)
+  process.exit(-1)
+})
+
+// On any connect (first connection) check to see if we have our table defined
+pgPool.on('connect', (client) => {
+  console.log('Seeing if our table exists and creating if it does not..')
+  client
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .then((res) => console.log('RESULT OF CREATE TABLE IS ', res))
+    .catch((err) => console.log('*** ERROR!!! ***', err))
+})
+
+// Get a client connection
+console.log('API -- Forcing a client connection')
+pgPool.connect().then((client) => (pgClient = client))
+
+// Redis Client Setup
+const redis = require('redis')
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+})
+const redisPublisher = redisClient.duplicate()
+
+// Express route handlers
+
+app.get('/', (req, res) => {
+  res.send('Hi')
+})
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values')
+
+  res.send(values.rows)
+})
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values)
+  })
+})
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high')
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!')
+  redisPublisher.publish('insert', index)
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
+
+  res.send({ working: true })
+})
+
+app.listen(5000, (err) => {
+  console.log('Listening')
+})
